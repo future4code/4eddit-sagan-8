540fb3aa9d9731eb70d1c365a34dbc85791c1717
Nadia Carvalho <carvalho.nadiacaroline@gmail.com>
2020-04-01 18:34:28 -0300

    rotas feitas para login e postDetail

--- src/containers/Router/index.js
+++ src/containers/Router/index.js
@@ -5,6 +5,7 @@ import LoginPage from "../LoginPage";
 import RegisterPage from "../RegisterPage";
 import HomePage from "../HomePage";
 import PostFeed from "../PostFeed/PostFeed";
+import PostDetail from "../PostDetail/PostDetail";
 
 
 
@@ -14,7 +15,8 @@ export const routes = {
   root: "/",
   registerPage: "/register",
   postFeed:"/postfeed",
-  login: "/login"
+  login: "/login",
+  PostDetail: "/postdetail"
 };
 
 function Router(props) {
@@ -25,11 +27,12 @@ function Router(props) {
         <Route exact path={routes.root} component={HomePage} />
         <Route exact path={routes.registerPage} component={RegisterPage} />
         <Route exact path={routes.postFeed} component={PostFeed} />
-        <Route exact path={routes.login} component={LoginPage} />
-
+        <Route exact path={routes.LoginPage} component={LoginPage} />
+        <Route exact path={routes.PostDetail} component={PostDetail} />
       </Switch>
     </ConnectedRouter>
   );
 }
 
 export default Router;
+
