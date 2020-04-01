56e5d21eb0fc2fa9d9ff1a8dc6957f8f3d2c422f
Nadia Carvalho <carvalho.nadiacaroline@gmail.com>
2020-04-01 18:28:13 -0300

    criaçao dos arquivos para o PostDetail

---
+++ src/containers/PostDetail/PostComments.js
@@ -0,0 +1,22 @@
+class PostComments extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+
+  }
+
+  render() {
+    return (
+      <div>
+        oie
+      </div>
+    );
+  }
+}
+
+PostComments.propTypes = {
+
+};
+
+export default PostComments;
---
+++ src/containers/PostDetail/PostDetail.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PostCard from './PostCard'
+
+
+class PostDetail extends React.Component {
+    render() {
+    return (
+     <PostCard />
+    );
+  }
+}
+
+PostDetail.propTypes = {
+
+};
+
+export default PostDetail;
